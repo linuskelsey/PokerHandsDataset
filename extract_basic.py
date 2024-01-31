94b5d12463a4deb3f2028b696428a7bb389d119c
linuskelsey <alfred.linus.kelsey@gmail.com>
2024-01-31 18:05:23 +0000

    Add new file to extract only basic hand info and winners

---
+++ extract_basic.py
@@ -0,0 +1,28 @@
+import json
+
+fout = open('hands_basic.json', 'w')
+fout.write('[')
+total = sum(1 for line in open('hands_valid.json'))
+with open('hands_valid.json', 'r') as f:
+    # Running total for number of rounds processed
+    curr = 0
+    line = f.readline()
+    while line:
+        hand = json.loads(line)
+        # Clean to only include whether players won or not
+        for player in hand['players']:
+            player['won'] = 0
+            if player['winnings'] > 0:
+                player['won'] = 1
+
+            del player['user'], player['bets'], player['bankroll'], player['action'], player['pos'], player['winnings']
+
+        del hand['game'], hand['dealer'], hand['pots'], hand['time']
+
+        curr += 1
+        fout.write(json.dumps(hand) + ',\n')
+        print('{} out of {} processed. {:.2%} done.'.format(curr, total, curr/total), end='\r')
+        line = f.readline()
+fout.write(']')
+
+print('\n    NOTE: Remove the trailing comma in hands_basic.json.\n')
